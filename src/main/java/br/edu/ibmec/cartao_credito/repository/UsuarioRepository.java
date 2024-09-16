7b2dbc0d6f93257837c6d131c6b65a7d656f1afa
Rafael Cruz <rafael.cruz@professores.ibmec.edu.br>
2024-09-16 09:06:10 -0300

    Adicionando configuração e repositorio

---
+++ src/main/java/br/edu/ibmec/cartao_credito/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ibmec.cartao_credito.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ibmec.cartao_credito.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    
+}
